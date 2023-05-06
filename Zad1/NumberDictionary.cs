af21ceca7c195daabb12005bee63c8e22d32c544
kszurgot <kamil.szurgot@int.pl>
2023-05-06 10:31:54 +0200

    Add project files.

---
+++ Zad1/NumberDictionary.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    internal static class NumberDictionary
+    {
+        public static Dictionary<int, string> Units { get; private set; } = new()
+        {
+            { 1, "jeden" },
+            { 2, "dwa" },
+            { 3, "trzy" },
+            { 4, "cztery" },
+            { 5, "pięć" },
+            { 6, "sześć" },
+            { 7, "siedem" },
+            { 8, "osiem" },
+            { 9, "dziewięć" }
+        };
+
+        public static Dictionary<int, string> Teen { get; private set; } = new ()
+        {
+            { 1, "jedenaście" },
+            { 2, "dwanaście" },
+            { 3, "trzynaście" },
+            { 4, "czternaście" },
+            { 5, "piętnaście" },
+            { 6, "szesnaście" },
+            { 7, "siedemnaście" },
+            { 8, "osiemnaście" },
+            { 9, "dziewietnaście" }
+        };
+
+        public static Dictionary<int, string> Tens { get; private set; } = new()
+        {
+            { 1, "dziesięć" },
+            { 2, "dwadzieścia" },
+            { 3, "trzydzieści" },
+            { 4, "czterdzieści" },
+            { 5, "pięćdziesiąt" },
+            { 6, "sześćdziesiąt" },
+            { 7, "siedemdziesiąt" },
+            { 8, "osiemdziesiąt" },
+            { 9, "dziewięćdziesiąt" }
+        };
+
+        public static Dictionary<int, string> Hundred { get; private set; } = new()
+        {
+            { 1, "sto" },
+            { 2, "dwieście" },
+            { 3, "trzysta" },
+            { 4, "czterysta" },
+            { 5, "pięćset" },
+            { 6, "sześćset" },
+            { 7, "siedemset" },
+            { 8, "osiemset" },
+            { 9, "dziewięćset" }
+        };
+
+        public static Dictionary<int, string> Thousands { get; private set; } = new()
+        {
+            {1, "tysiąc" },
+            {2, "tysiące" },
+            {3, "tysiące" },
+            {4, "tysiące" },
+            {5, "tysiący" },
+            {6, "tysiący" },
+            {7, "tysiący" },
+            {8, "tysiący" },
+            {9, "tysiący" }
+        };
+    }
+}
---
+++ Zad1/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? input;
+
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę:");
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out _) || input.Length > 4)
+                {
+                    Console.WriteLine("Nieprawidłowa liczba\n");
+                    continue;
+                }
+
+                break;
+            };
+
+            input = input.PadLeft(4, '0');
+
+            var inputResult = GetNumberInWrittenForm(input);
+
+            Console.WriteLine();
+            Console.WriteLine($"Liczba zapisana słownie: {inputResult}");
+            Console.WriteLine($"Liczba znaków: {inputResult.Replace(" ", string.Empty).Length}");
+
+            int result = 0;
+            for(int i = 1; i <= 1000;  i++)
+            {
+                result += GetNumberInWrittenForm(i.ToString())
+                    .Replace(" ", string.Empty)
+                    .Length;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Liczba znaków do zapisania liczb od 1 do 1000: {result}");
+        }
+
+        private static string GetNumberInWrittenForm(string number)
+        {
+            string result = "";
+            string value;
+
+            number = number.PadLeft(4, '0');
+
+            var (thousands, hundreds, tens, units, _) = number.Select(n => n - '0');
+
+            if (NumberDictionary.Thousands.TryGetValue(thousands, out value))
+            {
+                if (thousands > 1) {
+                    result += $" {NumberDictionary.Units[thousands]}";
+                }
+
+                result += $" {value}";
+            }
+
+            if (NumberDictionary.Hundred.TryGetValue(hundreds, out value))
+            {
+                result += $" {value}";
+            }
+
+            if (tens == 1 && units != 0)
+            {
+                if (NumberDictionary.Teen.TryGetValue(tens, out value))
+                {
+                    result += $" {value}";
+                }
+
+            }
+            else
+            {
+                if (NumberDictionary.Tens.TryGetValue(tens, out value))
+                {
+                    result += $" {value}";
+                }
+
+                if (NumberDictionary.Units.TryGetValue(units, out value))
+                {
+                    result += $" {value}";
+                }
+            }
+
+            //Console.WriteLine($"{thousands} {hundreds} {tens} {units}");
+
+            return result.Substring(1);
+        }
+    }
+
+  
+}
